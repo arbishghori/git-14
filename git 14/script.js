af1d73fbea84f236d958f191f24d21b0a54b34fc
arbishghori <128428077+arbishghori@users.noreply.github.com>
2023-08-18 11:43:25 -0700

    Add files via upload

---
+++ git 14/script.js
@@ -0,0 +1,9 @@
+// there is 2 ways to find the date 
+
+var today = new Date();
+
+var doomsday = new Date("June 30, 2035");
+console.log(new Date);
+
+var d = new Date("July 21, 1983 13:25:00");
+console.log(new Date);
